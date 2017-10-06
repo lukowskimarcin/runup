6dfc7c7e8cbf284bb9ee0cc7dce2751c4e5697c2
Marcin <Marcin@pclukowski>
2017-10-06 11:39:31 +0200

    .

--- runup/src/main/java/runup/Application.java
+++ runup/src/main/java/runup/Application.java
@@ -2,18 +2,27 @@ package runup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
 }
